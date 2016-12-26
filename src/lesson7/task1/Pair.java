d50dd17c0b326ccfdb382bd00a759cecbb538246
0vv0 <softina@ukr.net>
2016-12-26 22:34:49 +0200

    lesson7.task1 - Initial

---
+++ src/lesson7/task1/Pair.java
@@ -0,0 +1,27 @@
+package lesson7.task1;
+
+/**
+ * Created by Oleksii.Sergiienko on 12/26/2016.
+ */
+public class Pair<L, R> {
+    private final L left;
+    private final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    L getLeft(){
+        return left;
+    }
+
+    R getRight(){
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left +", " + right +")";
+    }
+}
---
+++ src/lesson7/task1/Runner.java
@@ -0,0 +1,19 @@
+package lesson7.task1;
+
+/**
+ * Created by Oleksii.Sergiienko on 12/26/2016.
+ */
+public class Runner {
+//    Создать класс Pair<L, R>, хранящий пару значений (L left, R right).
+// Объекты класса Pair должны быть неизменяемые (Immutable).
+// Создать клиентский класс, демонстрирующий работу Pair.
+
+    public static void main(String[] args) {
+        Pair<Integer, Integer> pairOfInts = new Pair<>(1, 12);
+        System.out.println(pairOfInts);
+        System.out.println(pairOfInts.getLeft());
+        System.out.println(pairOfInts.getRight());
+
+        System.out.println(new Pair<String, Integer>("Hello", 12));
+    }
+}
