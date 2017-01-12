956254b06c818db640bcce05b993cc80695349bf
0vv0 <softina@uk.net>
2017-01-12 13:12:34 +0200

    added some test for lesson2.task2

--- src/lesson2/task2/BookTest.java
+++ src/lesson2/task2/BookTest.java
@@ -14,7 +14,8 @@ public class BookTest {
         book = new Book.Builder().build();
         assert book.getId()==0;
         assert book.getId()==0;
-        assert new Book.Builder().build().getId()==1;
+        Book newBook = new Book.Builder().build();
+        assert newBook.getId()==1;
         book=new Book.Builder().build();
         assert book.getId()==2;
     }
@@ -24,11 +25,34 @@ public class BookTest {
         Book book;
         book = new Book.Builder().build();
         assert book.getTitle().equals("");
+        book = new Book.Builder().setTitle(null).build();
+        assert book.getTitle().equals("");
+        book = new Book.Builder().setTitle("").build();
+        assert book.getTitle().equals("");
+        book = new Book.Builder().setTitle("The Title").build();
+        assert book.getTitle().equals("The Title");
+        assertFalse(book.getTitle().equals("the title"));
     }
 
     @Test
     public void getAuthor() throws Exception {
+        Book book;
+        book = new Book.Builder().build();
+        assert book.getAuthor().getNick().equals("");
+        assert book.getAuthor().getName().equals("");
+        assert book.getAuthor().getSurname().equals("");
+        assert book.getAuthor().getYearOfBirth()==0;
 
+        book = new Book.Builder().setAuthor(null).build();
+        assert book.getAuthor().getNick().equals("");
+        assert book.getAuthor().getName().equals("");
+        assert book.getAuthor().getSurname().equals("");
+        assert book.getAuthor().getYearOfBirth()==0;
+
+        Author author = new Author("The Nick", "The Name", "The Surname", 1980);
+        book = new Book.Builder().setAuthor(author).build();
+        assert book.getAuthor().equals(author);
+        assertFalse(book.getAuthor().equals(new Author("The Nick", "The Name", "The Surname", 1980)));
     }
 
     @Test
