5f3099320f2e9b4983ec984d726340edd48076c5
0vv0 <softina@ukr.net>
2017-01-29 14:07:42 +0200

    lesson11

--- src/lesson11/task1/Factorial.java
+++ src/lesson11/task1/Factorial.java
@@ -5,71 +5,36 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.time.LocalTime;
 
 /**
  * Created by default on 1/29/2017.
  */
 public class Factorial implements Runnable {
-    public static final String PATH = "src\\lesson11\\task1\\files\\";
-    private static final int K = 1000;
-    private static final int N = 100;
+    public static final String PATH = "src\\lesson11\\task1\\files";
     private final int base;
-    protected BigInteger factorial;
+    private final String fileName;
 
     public Factorial(int k) {
         this.base = k;
+        this.fileName = PATH + "\\factorial " + base;
     }
 
-    //1. Написать метод вычисления факториала.
-//2. Написать метод вывода значения факториала в файл.
-//3. Написать метод, в котором N потоков под индексами i = 1..N параллельно вычисляют факториал числа (K * i)
-// и записывают в файл с названием "factorial K * i" (например, для K = 1000, "factorial K", "factorial 2K", ....)
-//    Дополнительно:
-//            4. Произвести бенчмарк и сравнить скорость работы многопоточного и аналогичного однопоточного кода.
-//    Протестировать программу для N = 100, K = 1000.
-    public static void main(String[] args) throws IOException, InterruptedException {
-        LocalTime lt1;
-        LocalTime lt2;
-//      clean directory/delete all files
-        for (int i = 0; i <= N; i++) {
-            new File(PATH + "factorial " + K * i).delete();
+    public static void cleanDirectory() {
+        File[] files = new File(PATH).listFiles();
+        if (files != null) {
+            for (File file : files) {
+                file.delete();
+            }
         }
-//        get current time for benchmark
-        lt1 = LocalTime.now();
-//        starts N threads and run factorial calculation in them
-        for (int i = 0; i <= N; i++) {
-            new Thread(new Factorial(K * i)).start();
-        }
-//        wait for last file with factorial is written
-        while (!new File(PATH + "factorial " + K * N).exists()) {
-            Thread.sleep(1000);
-        }
-//        get current time for benchmark
-        lt2 = LocalTime.now();
-        System.out.println("parallel time == " + (lt2.toNanoOfDay() - lt1.toNanoOfDay()) / 1000_000 + " milliseconds");
 
-//      clean directory/delete all files
-        for (int i = 0; i <= N; i++) {
-            new File(PATH + "factorial " + K * i).delete();
-        }
-//        get current time for benchmark
-        lt1 = LocalTime.now();
-//        calculate factorials and write them to files sequentially
-        for (int i = 0; i < N; i++) {
-            new Factorial(K * i).run();
-        }
-//        get current time for benchmark
-        lt2 = LocalTime.now();
-        System.out.println("sequential time == " + (lt2.toNanoOfDay() - lt1.toNanoOfDay()) / 1000_000 + " milliseconds");
     }
 
-    public static BigInteger get(int i) {
+    public BigInteger get(int i) {
         if (i < 2) {
             return BigInteger.ONE;
         }
         BigInteger m = BigInteger.ONE;
-        for (int y = 1; y <= i; y++) {
+        for (int y = 2; y <= i; y++) {
             m = m.multiply(BigInteger.valueOf(y));
         }
         return m;
@@ -77,17 +42,15 @@ public class Factorial implements Runnable {
 
     @Override
     public void run() {
-        factorial = get(base);
-
         try {
-            write(new File(PATH + "factorial " + base));
+            write(get(base));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void write(File file) throws IOException {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+    public void write(BigInteger factorial) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)))) {
             bw.write(factorial.toString());
         }
     }
---
+++ src/lesson11/task1/Runner.java
@@ -0,0 +1,49 @@
+package lesson11.task1;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+/**
+ * Created by default on 1/29/2017.
+ */
+public class Runner {
+    private static final int K = 1000;
+    private static final int N = 100;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        parallel();
+        sequential();
+        parallel();
+        sequential();
+
+    }
+
+    private static void parallel() throws InterruptedException {
+        LocalTime lt1;
+        LocalTime lt2;//parallel block
+        Factorial.cleanDirectory();
+        Thread[] threads = new Thread[N + 1];
+        lt1 = LocalTime.now();
+        for (int i = 0; i <= N; i++) {
+            threads[i] = new Thread(new Factorial(K * i));
+            threads[i].start();
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+        lt2 = LocalTime.now();
+        System.out.println("parallel time == " + (lt2.toNanoOfDay() - lt1.toNanoOfDay()) / 1000_000 + " milliseconds");
+    }
+
+    private static void sequential() {
+        LocalTime lt1;
+        LocalTime lt2;//sequential block
+        Factorial.cleanDirectory();
+        lt1 = LocalTime.now();
+        for (int i = 0; i < N; i++) {
+            new Factorial(K * i).run();
+        }
+        lt2 = LocalTime.now();
+        System.out.println("sequential time == " + (lt2.toNanoOfDay() - lt1.toNanoOfDay()) / 1000_000 + " milliseconds");
+    }
+}
