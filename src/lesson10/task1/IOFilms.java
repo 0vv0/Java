7787f34eb276c5d487a7b1c0af49b05d1c6b859e
0vv0 <softina@uk.net>
2017-01-31 10:13:13 +0200

    lesson10

--- src/lesson10/task1/IOFilms.java
+++ src/lesson10/task1/IOFilms.java
@@ -7,13 +7,5 @@ import java.io.IOException;
  * Created by default on 1/28/2017.
  */
 public interface IOFilms {
-//    Создать классы:
-//            1. Film: название, год выпуска, список имен актеров, список жанров.
-//2. IOFilms: List<Film> films (возможно, extends Films). Позволяет выполнять запросы:
-//    a) Запись фильмов в текстовый файл.
-//            b) Чтение фильмов из текстового файла.
-//            c) Запись фильмов в бинарный файл (сериализация).
-//    d) Чтение фильмов из бинарного файла (десериализация).
-
     void writeTo(File file) throws IOException;
 }
